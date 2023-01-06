139b186a69d821c4a2e8fb64aa202f372c396865
raian-shahrear <raianshahrearbd@gmail.com>
2023-01-06 23:29:16 +0600

    home-btn added to the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Error = () => {
   return (
-    <div className="h-screen w-screen bg-white flex justify-center items-center">
+    <div className="h-screen w-screen bg-white flex justify-center items-center relative">
       <Image src="/resources/404.gif" width={800} height={800} />
+      <p className="absolute bottom-40 sm:bottom-20 md:bottom-16"><Link href="/" className="btn">Back to Home</Link></p>
     </div>
   );
 };
